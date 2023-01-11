bab399914ef54d223dece91162bdccee4b76eb70
Arunansu Pattanayak <arunansu@hotmail.com>
2023-01-10 21:03:36 -0500

    replaced with html

---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+<footer>
+	<div class="container">
+		<div class="footer-box">
+			<div class="row">
+
+			<div class="col-md-7">
+				<div class="heading">
+					<h3>ABOUT</h3>
+				</div>
+				<div class="mail-links">
+					<ul>
+						<li>
+							<a href="mailto:info@tipsora.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@tipsora.com</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<div class="heading">
+					<h3>LINKS</h3>
+				</div>
+				<div class="nav-links">
+					<ul>
+						<li>
+							<a href="<?php echo $path;?>">Home</a>
+						</li>
+						<li>
+							<a href="<?php echo $path;?>consulting">Consulting</a>
+						</li>
+						<li>
+							<a href="<?php echo $path;?>training">Training</a>
+						</li>
+						<li>
+							<a href="<?php echo $path;?>recruitment">Recruitment</a>
+						</li>
+						<li>
+							<a href="<?php echo $path;?>about-us">About Us</a>
+						</li>
+						<li>
+							<a href="<?php echo $path;?>contact">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+			<div class="col-md-3">
+        <div class="iconwrp">
+            <ul>
+              <li>
+                <i class="fa-brands fa-facebook-f"></i>
+              </li>
+              <li>
+                <i class="fa-brands fa-instagram insta-icon"></i>
+              </li>
+              <li>
+                <i class="fa-brands fa-twitter twitter-icon"></i>
+              </li>
+              <li>
+                <i class="fa-brands fa-linkedin link-icon"></i>
+              </li>
+            </ul>  
+        </div>
+      </div>
+		</div>
+		</div>
+		</div>
+	</div>
+	<div class="btm-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<div class="txtwrp">
+					<p>© Copyrights, 2022</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="btnwrp">
+					<a href="<?php echo $path;?>faqs">Terms & Conditions</a>
+					<a href="javascript:;">Privacy Policy</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</footer>
+
+
+
+
+
+
+
+
+
+</main>
+
+
+<script src="<?php echo $basesurl;?>js/mlib.js"></script> 
+<script src="<?php echo $basesurl;?>js/functions.js"></script> 
+<script type="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script type="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+
+<script>
+	new WOW.init();
+</script>
+
+
+
